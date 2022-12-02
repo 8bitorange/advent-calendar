e66dbfe4550ebb85b890767fa2fa85cc4baa6fa1
Matt Caron <sirmattcaron@gmail.com>
2022-12-02 13:47:58 -0500

    day 2, problem 2

--- dec-2/index.js
+++ dec-2/index.js
@@ -1,3 +1,4 @@
+const assert = require('node:assert/strict');
 const fs = require('fs');
 
 const input = fs.readFileSync('./input.txt', 'utf8');
@@ -5,45 +6,51 @@ const input = fs.readFileSync('./input.txt', 'utf8');
 const games = input.split('\n').filter(n => n);
 
 class Move {
-    constructor (name, score, beats) {
+    constructor (name, score, beats, losesTo) {
         this.score = score;
         this.name = name;
         this.beats = beats;
+        this.losesTo = losesTo;
     }
 }
 
-const rock = new Move('rock', 1, 'scissors');
-const paper = new Move('paper', 2, 'rock');
-const scissors = new Move('scissors', 3, 'paper');
+const rock = new Move('rock', 1, 'scissors', 'paper');
+const paper = new Move('paper', 2, 'rock', 'scissors');
+const scissors = new Move('scissors', 3, 'paper', 'rock');
+
+const availablePlays = {rock, paper, scissors};
 
 const play = {
     'A': rock,
     'B': paper,
     'C': scissors,
-    'X': rock,
-    'Y': paper,
-    'Z': scissors
+    'X': 'lose',
+    'Y': 'draw',
+    'Z': 'win'
 };
 
 const scores = {
     'win': 6,
     'draw': 3,
-    'loss': 0
+    'lose': 0
 };
 
 const decider = (match) => {
     const [opponent, me] = match.split(' ');
-    console.log(`Opponent plays ${play[opponent].name} vs Me with ${play[me].name}`);
-    if (play[opponent].beats === play[me].name) {
-        console.log(`Lose with ${play[me].name}`);
-        return scores.loss + play[me].score;
-    } else if (play[opponent].name === play[me].name) {
-        console.log(`Draw with ${play[me].name}`);
-        return scores.draw + play[me].score;
-    } else {
-        console.log(`Win with ${play[me].name}`);
-        return scores.win + play[me].score;
+    console.log(`Opponent plays ${play[opponent].name} and I should ${play[me]}`);
+    let result;
+    switch (play[me]) {
+        case 'draw':
+            result = availablePlays[play[opponent].name].score + scores.draw;
+            break;
+        case 'win':
+            result = availablePlays[play[opponent].losesTo].score + scores.win;
+            break;
+        case 'lose':
+            result = availablePlays[play[opponent].beats].score + scores.lose;
+            break;
     }
+    return result;
 }
 
 let myScore = 0;
